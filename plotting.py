4dcd15162cef80033217c2b60fd1856bac7115d6
pillai-amal <pillai_amal@hotmail.com>
2023-04-15 20:09:11 +0200

    basic commit

--- plotting.py
+++ plotting.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("data.csv") # reading the dataset into a pandas dataframe 
-x = 'Time'
-y_1 = 'Channel A'
-y_2 = 'Channel B'
-fig, ax = plt.subplots()
-#df.plot(x='Time', y='Channel A', kind='line')
-#df.plot(x='Time', y='Channel B', kind='line')
+#loading csv into pandas dataframe
+df = pd.read_csv("data.csv") 
 
+#defining axis
+x =  df(["Time"]).tonumpy() 
+y_1 = df(["Channel A"]).tonumpy() 
+y_2 = df(["Channel A"]).tonumpy()
+
+#plotting 
+
+fig, ax = plt.subplots()
 ax.plot(x, y_1)
 ax.plot(x, y_2)
 plt.show()
